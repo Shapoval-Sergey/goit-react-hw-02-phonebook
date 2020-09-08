5b66e569baabc5fa452c75ef5e534082d479ffb6
Sergey <shapa92sergey@gmail.com>
2020-09-08 16:32:06 +0300

    Updates

---
+++ precache-manifest.3f24f19be32b9bb20c178ecb64ed1c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a518bf882958ea32f70a1866ae2cd361",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c414447fdf53e9c7d75d",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.7b88d7af.chunk.css"
+  },
+  {
+    "revision": "8fc321acf0aeb6e02acb",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ebfb8f7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ebfb8f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c414447fdf53e9c7d75d",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.e456d8fc.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
